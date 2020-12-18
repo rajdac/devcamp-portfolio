baa986a4e3a72cf95ebf78d24184eaea8759071f
Rajeev Singh <rajdac@gmail.com>
2020-12-18 10:52:13 +0530

    added new and create actions for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,19 @@ class PortfoliosController < ApplicationController
 	def index
 		@portfolio_items = Portfolio.all
 	end	
+
+	def create
+	    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title,:subtitle,:body))
+	    respond_to do |format|
+	      if @portfolio_item.save
+	        format.html { redirect_to portfolios_path, notice: "Your portfolio is saved" }
+	      else
+	        format.html { render :new }
+	      end
+	    end  
+    end
+
+	def new
+		@portfolio_item = Portfolio.new
+	end
 end
