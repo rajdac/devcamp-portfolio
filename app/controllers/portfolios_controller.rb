4778988645b4b21c3013153553a3d1be956cb66b
Rajeev Singh <rajdac@gmail.com>
2020-12-23 10:53:22 +0530

    added edit and show action refactored start server

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,10 +1,15 @@
 class PortfoliosController < ApplicationController
+	before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@portfolio_items = Portfolio.all
 	end	
 
+	def show
+    end
+
 	def create
-	    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title,:subtitle,:body))
+	    @portfolio_item = Portfolio.new(portfolio_params)
 	    respond_to do |format|
 	      if @portfolio_item.save
 	        format.html { redirect_to portfolios_path, notice: "Your portfolio is saved" }
@@ -17,4 +22,43 @@ class PortfoliosController < ApplicationController
 	def new
 		@portfolio_item = Portfolio.new
 	end
+    
+    def show
+   
+       
+    end
+
+
+    def edit
+   
+       
+    end	
+
+    def update
+	    respond_to do |format|
+	      if @portfolio_item.update(portfolio_params)
+	        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+	      else
+	        format.html { render :edit }
+	      end
+	    end
+  	end
+
+  	def destroy
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' }
+    end
+  end
+
+  	private 
+
+  	def set_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
+  	end
+
+  	def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle,  :body)
+  	end
+
 end
--- run_server.sh
+++ run_server.sh
@@ -6,22 +6,39 @@ BUNDLER_VERSION="2.0"
 RUN_MODE="rebuild"
 APP_BASE_PATH="/home/msuser1/workspace/projects"
 
-RAILS_ENV="development"
+#RAILS_ENV="development"
 RAILS_VERSION="5.1.6"
 APP_IMAGE="app1:v1"
 APP_CONTAINER="app1"
 GEM_HOME=${APP_BASE_PATH}/gem_home
-
-
+RAILS_ENV="${RAILS_ENV:-development}"
 #${RAILS_ENV:-development}
 
 
 echo "Stop container if present..................."
-#docker stop $APP_CONTAINER
+OLD_APP_CONTAINER="$(docker ps --all --quiet --filter=name="$APP_CONTAINER")"
+if [ -n "$OLD_APP_CONTAINER" ]; then
+  echo "detected running app container ${APP_CONTAINER}"	
+  docker stop $OLD_APP_CONTAINER && docker rm $OLD_APP_CONTAINER
+fi
+
 
 echo "Build the docker..........................."
 docker build -t ${APP_IMAGE} --build-arg APP_BASE_IMAGE=$APP_BASE_IMAGE --build-arg APP_NAME=$APP_NAME  -f docker/Dockerfile.app .
 
-#sudo chown -R $USER:$USER .
+# pkexec visudo
+# https://www.suse.com/support/kb/doc/?id=000016906
+#Edit the /etc/sudoers file to include the following:
 
-docker run -it --security-opt apparmor=unconfined --cap-add=SYS_PTRACE -p 3000:3000 --rm -e RAILS_VERSION=$RAILS_VERSION -e BUNDLER_VERSION=$BUNDLER_VERSION -e APP_NAME=$APP_NAME  -e RUN_MODE=$RUN_MODE -e DOCKER_CONTAINER=app -e APP_MODE=web -e RAILS_ENV=$RAILS_ENV --volume=${APP_BASE_PATH}/${APP_NAME}:/opt/${APP_NAME} --volume=${GEM_HOME}:/usr/local/bundle/ --name ${APP_CONTAINER} ${APP_IMAGE}
+	# User privilege specification
+	#root        ALL=(ALL) ALL
+	# <user_name>    ALL=NOPASSWD: ALL
+
+# https://askubuntu.com/questions/425754/how-do-i-run-a-sudo-command-inside-a-script
+# added msuser to sudo users as below
+#sudo tee /etc/sudoers.d/$USER <<END
+#END
+
+sudo chown -R $USER:$USER .
+
+docker run -it --security-opt apparmor=unconfined --cap-add=SYS_PTRACE --rm -p 3000:3000 -e RAILS_VERSION=$RAILS_VERSION -e BUNDLER_VERSION=$BUNDLER_VERSION -e APP_NAME=$APP_NAME  -e RUN_MODE=$RUN_MODE -e DOCKER_CONTAINER=app -e APP_MODE=web -e RAILS_ENV=$RAILS_ENV --volume=${APP_BASE_PATH}/${APP_NAME}:/opt/${APP_NAME} --volume=${GEM_HOME}:/usr/local/bundle/ --name ${APP_CONTAINER} ${APP_IMAGE}
